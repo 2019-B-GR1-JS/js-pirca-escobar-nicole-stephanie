a7264028ca3e769961fa92365bc3aad122a42418
Nicole Pirca <nicole.pirca@epn.edu.ec>
2019-11-25 18:08:42 -0500

    Angular2

--- 03-Angular/componentes/src/app/app.component.ts
+++ 03-Angular/componentes/src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'componentes';
+  //objetos
+  arregloObjtos =[
+    {
+      id: 1, //Number
+      "nombre": "Nicole", //String
+      apellido: 'Pirca', //string
+      sueldo: 12.12, // number
+      casado: false, //boolean
+      comida: "https://statics-cuidateplus.marca.com/sites/default/files/styles/natural/public/comida_2.jpg?itok=HmukTYgl",
+      carro: null,
+      ojos: undefined,
+    },
+    {
+      id: 2, //Number
+      "nombre": "Thais", //String
+      apellido: 'Escobar', //string
+      sueldo: 12.12, // number
+      casado: false, //boolean
+      comida: "https://d25rq8gxcq0p71.cloudfront.net/language-guide/758/pepperoni%20pizza.jpg",
+      carro: null,
+      ojos: undefined,
+    },
+    {
+      id: 3, //Number
+      "nombre": "Victor", //String
+      apellido: 'Lema', //string
+      sueldo: 12.12, // number
+      casado: false, //boolean
+      comida: "https://dam.ngenespanol.com/wp-content/uploads/2019/03/desperdicio-de-comida.png",
+      carro: null,
+      ojos: undefined,
+    }
+  ]
+  imprimirSueldo(sueldo: string){
+    console.log(sueldo);
+  }
 }
--- 03-Angular/componentes/src/app/mi-primer-componente/mi-primer-componente.component.ts
+++ 03-Angular/componentes/src/app/mi-primer-componente/mi-primer-componente.component.ts
@@ -1,18 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-mi-primer-componente',
   templateUrl: './mi-primer-componente.component.html',
   styleUrls: ['./mi-primer-componente.component.css']
 })
+
+
 export class MiPrimerComponenteComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  public titulo: string;
+  @Input()
+  imagen: string;
+  @Input()
+  public textoImagen: string;
+  @Input()
+  public textoButton: string;
 
-  ngOnInit() {
+  @Output () //EVENTO
+  public cambioSueldo = new EventEmitter(); // definicion de mi evento desde el angular core
+
+  constructor() {
+    console.log('Instanciando');
+  }  // instancia a la clase (nOS AYUDA EL MISMO ANGULAR)
+// esto es una interfaz que se implementa
+
+
+  ngOnInit() { // La clase esta lista
+    console.log('EST LISTO');
+    console.log('Esta listo');
+  }
+
+  ngOnDestroy(): void { // la calase esta destruida
+
   }
 
   saludar(){
     alert("HOLAAA");
   }
+
+  //class (etiqueta html) (web-component)
+  // instanciar a la clase
+  // clase esta lista
+  // clase se destruye
+aumentarSueldo(){
+    this.textoButton =(Number(this.textoButton) +1).toString(); //transformar
+    this.cambioSueldo.emit(this.textoButton);
+}
+aumentarImagen(){
+    this.imagen=(Number(this.imagen)+1).toString();
+    console.log(this.imagen)
+
+}
 }
