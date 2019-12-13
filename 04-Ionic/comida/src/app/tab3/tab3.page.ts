783d0f33e9956539be58f8eaaab4ebe8cd484758
Nicole Pirca <nicole.pirca@epn.edu.ec>
2019-12-13 18:01:23 -0500

    ionic

---
+++ 04-Ionic/comida/src/app/tab3/tab3.page.ts
@@ -0,0 +1,24 @@
+import {Component} from '@angular/core';
+import {NgForm} from "@angular/forms";
+
+@Component({
+    selector: 'app-tab3',
+    templateUrl: 'tab3.page.html',
+    styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+
+    usuario = {
+        nombre: '',
+        apellido: ''
+    };
+
+    constructor() {
+    }
+
+    crearUsuario(formulario: NgForm) {
+        // el # podemos utilizar
+        console.log(formulario);
+    }
+
+}
